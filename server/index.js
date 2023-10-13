49822ebda9bac66c79b0bd2d3913052714f64472
ThanawatBundasak <s6303051633018@kmutnb.ac.th>
2023-10-13 20:43:09 +0700

    form-get and form-post
    form-get.js(test)
    form-post.js(work)

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const port = 8000
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(express.static('public'))
+
+app.get('/api/form-get', (request, response) => {
+    let t = request.query.target || ''
+    let k = request.query.kw || ''
+    let n = parseInt((Math.random() * 1000))
+    let r = {
+        target: t,
+        kw: k,
+        results: n
+    }
+    response.json(r)
+})
+
+app.listen(port, () => {
+    console.log('Server listening on port ' + port)
+})
